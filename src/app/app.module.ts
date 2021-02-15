d6c7b028acb8aab499660a14a8fff7d3a3cdd00a
JosueBotella <josuebotella@gmail.com>
2021-02-15 16:37:08 +0100

    kudvenkat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,6 @@ import { AppComponent } from "./app.component";
 import { EmployeeListComponent } from "./employee-list/employee-list.component";
 import { employeeTitle } from "./employee-list/employeeTitle.pipe";
 import { EmployeeCountComponent } from "./employee-list/employeeCount.component";
-import { OthersComponent } from './others/others.component';
-import { SimpleComponent } from './simple/simple.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
@@ -15,9 +13,7 @@ import { SimpleComponent } from './simple/simple.component';
     AppComponent,
     EmployeeListComponent,
     employeeTitle,
-    EmployeeCountComponent,
-    OthersComponent,
-    SimpleComponent
+    EmployeeCountComponent
   ],
   bootstrap: [AppComponent]
 })
